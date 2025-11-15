c2795416ef57df4a49112c3874ae02d0bf15199d
Altamiro <altamiro_bruno@yahoo.com.br>
2025-11-15 13:59:22 -0300

    feat: adiciona empty card e atualiza nuxt ui

--- app/components/wallet/WalletList.vue
+++ app/components/wallet/WalletList.vue
@@ -1,15 +1,25 @@
 <template>
-  <ul>
+  <ul v-if="!walletsIsEmpty">
     <li v-for="item in WalletList" :key="item.id">
       <WalletCard can-delete class="mt-4 w-full" :wallet="item" />
     </li>
   </ul>
+  <UEmpty
+    v-else
+    class="mt-8"
+    variant="outline"
+    icon="i-lucide-wallet"
+    title="Sem carteiras."
+    description="Não há carteiras a serem mostradas aqui."
+  />
 </template>
 
 <script setup lang="ts">
-import { requestWallets } from '@/components/wallet/wallet.queries'
+import { requestWallets } from "@/components/wallet/wallet.queries";
 
-const { data: WalletList } = requestWallets(5 * 60 * 1000)
+const { data: WalletList} = requestWallets(5 * 60 * 1000);
+
+const walletsIsEmpty = computed(() => !WalletList.value?.length);
 </script>
 
 <style>
